e9d4d0aa67ec85cbca4e0a9386c1f88815d6f458
SAint7579 <vishwa20@outlook.com>
2020-05-04 03:58:30 +0530

    Fixed lexer function detection

--- Lexer/abc.java
+++ Lexer/abc.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public int f1(int a)
 {
+	return 1;
 }
 
 class Test
@@ -9,7 +10,7 @@ class Test
 public static void main(String args[])
 {
 int b;
-int a=12;
+int a=12.7;
 if(a==12)
 {
 ;
